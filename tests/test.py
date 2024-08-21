6a7a486628b47001049ef0aa2f33b833d76e0bec
hosanglee-kr <50106977+hosanglee-kr@users.noreply.github.com>
2024-08-21 21:01:50 +0900

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,16 @@
+
+import sys
+import os
 import pytest
+
+# Add the src directory to sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from Py_HHT_T2_001 import main
+
+
 #from src.a import main
-from src.Py_HHT_T2_001 import main
+#from src.Py_HHT_T2_001 import main
 
 def test_main(capsys):
     # Run the main function
