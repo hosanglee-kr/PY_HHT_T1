d3a6fd99035c6c90167152c1c9b60b1298bd197a
hosanglee-kr <50106977+hosanglee-kr@users.noreply.github.com>
2024-08-21 21:31:51 +0900

    Update Py_HHT_T2_001.py

--- src/Py_HHT_T2_001.py
+++ src/Py_HHT_T2_001.py
@@ -7,7 +7,7 @@ import threading
 import queue
 
 # Settings
-filename = '보트 모터 낮은음.wav'  # Path to your WAV file
+filename = 'boat1.wav'  # Path to your WAV file
 buffer_size = 44100  # Buffer size for processing (1 second of data)
 
 # EEMD object creation
